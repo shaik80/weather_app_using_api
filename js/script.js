32a504d88b22f15e3a2517bdb419ae6d4b61711e
shaik80 <sh.mudassir98@gmail.com>
2019-10-01 23:27:09 -0700

    changes

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ class Weatherdetails{
       if (degree == 0){
           let a = fetch("https://api.openweathermap.org/data/2.5/weather?q="+getcity+"&appid="+appid)
           .then(v => {
-            return v.json()
+            return console.log(v.json())
         })
         .then(v =>{
             console.log(imglink+v.weather[0].icon+".png")
